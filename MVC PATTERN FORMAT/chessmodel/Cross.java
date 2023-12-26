342169b5496e4bef81572d1497ae0bf6c5110661
1211104979 <1211104979@student.mmu.edu.my>
2023-12-26 10:46:18 +0800

    Added Cross piece and Player Class (Player Status = remaining piece and win condition)

---
+++ MVC PATTERN FORMAT/chessmodel/Cross.java
@@ -0,0 +1,15 @@
+package chessmodel;
+
+public class Cross extends Piece {
+    
+    public Cross(boolean white) {
+        super(white);
+        }
+
+    @Override
+    public boolean canMove() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+        
+}
---
+++ MVC PATTERN FORMAT/chessmodel/Player.java
@@ -0,0 +1,10 @@
+package chessmodel;
+
+public class Player {
+    
+    public Point point [] = new Point[7];
+    public Plus plus [] = new Plus[2];
+    public Hourglass hourglass [] = new Hourglass[7];
+    public Cross cross[] = new Cross[7];
+    public Sun sun;
+}
